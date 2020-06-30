62c2d3f8f31df7684960e262d6eecc984f2cb283
TajamHeng <oojinheng@gmail.com>
2020-07-01 00:26:38 +0800

    make year number dynamic

--- components/footer.vue
+++ components/footer.vue
@@ -3,9 +3,22 @@
     <div class="row justify-content-center mt-4 mb-2">
       <div class="col-12 col-md-8">
         <div class="p-3 bg-dark text-light text-center rounded gl-opac-80">
-          MMUCraft @ 2019
+          MMUCraft @ {{ year }}
         </div>
       </div>
     </div>
   </b-container>
 </template>
+
+<script>
+export default {
+  created () {
+    this.year = new Date().getFullYear()
+  },
+  data () {
+    return {
+      year: ''
+    }
+  }
+}
+</script>
