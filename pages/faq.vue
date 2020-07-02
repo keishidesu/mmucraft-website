d0af5a1ab1428e709d69e6d08eccfcef3909d107
manyimy <wong,manyi19@gmail.com>
2020-07-02 20:02:43 +0800

    Merge with upstream

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,9 +1,28 @@
 <template>
-  <div />
+  <div class="container-fluid">
+    <div class="row justify-content-center mt-5">
+      <div class="col-12 col-md-8 position-relative mt-5">
+        <div class="wow fadeInUp card bg-dark text-light">
+          <div class="card-header text-center text-white gl-font font-weight-bolder" style="font-size: 40px;text-shadow: 2px 6px 3px #000000;">
+            Frequently Asked Questions
+          </div>
+          <!-- <div class="accordion" id="accordionFAQ">
+            <% questions.forEach((item, id) => { %>
+              <%- include('layouts/components/question', {item: item, id: id}); %>
+            <% }); %>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+// import Question from '~/components/question'
 
-}
+// export default {
+//   components: {
+//     Question
+//   }
+// }
 </script>
