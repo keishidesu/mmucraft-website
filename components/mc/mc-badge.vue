421c5f62efc3f4ebba6215d9ebaa99ada3542a3e
keishidesu <caseytea0840@gmail.com>
2020-07-03 21:35:29 +0800

    added command page

---
+++ components/mc/mc-badge.vue
@@ -0,0 +1,15 @@
+<template>
+  <span class="gl-pointer badge badge-pill badge-info mr-2" data-toggle="tooltip" data-placement="top">
+    <slot />
+  </span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- components/rules/rules-board.vue
+++ components/rules/rules-board.vue
@@ -1,38 +1,34 @@
 <template>
-  <div class="container-fluid">
-    <div class="mt-5 row justify-content-center">
-      <div class="mt-5 col-12 col-md-8 position-relative text-light">
-        <div class="card animated animatedFadeInUp fadeInUp bg-dark shadow-lg">
-          <MCTitle class="mt-4" style="text-shadow: 2px 6px 3px #000000;">
-            {{ sectionTitle }}
-          </MCTitle>
-          <div class="card-body p-3">
-            <div class="text-center text-danger" style="font-size:1.1rem">
-              Any action performed by any player that any admin consider as a threat to community and server will result in permanent ban.<br>
-              Think of yourself and behave. Report to us if you found any harmful player.
-            </div>
-          </div>
-          <hr>
-          <div v-for="(rule, key) in rules" :key="`rule-${key}`" class="card-body">
-            <RulesCard
-              :title="rule.title"
-              :descs="rule.descs"
-            />
-            <hr>
-          </div>
-        </div>
+  <MCBoard>
+    <MCTitle class="mt-4" style="text-shadow: 2px 6px 3px #000000;">
+      {{ sectionTitle }}
+    </MCTitle>
+    <div class="card-body p-3">
+      <div class="text-center text-danger" style="font-size:1.1rem">
+        Any action performed by any player that any admin consider as a threat to community and server will result in permanent ban.<br>
+        Think of yourself and behave. Report to us if you found any harmful player.
       </div>
     </div>
-  </div>
+    <hr>
+    <div v-for="(rule, key) in rules" :key="`rule-${key}`" class="card-body">
+      <RulesCard
+        :title="rule.title"
+        :descs="rule.descs"
+      />
+      <hr>
+    </div>
+  </MCBoard>
 </template>
 
 <script>
 import { rules } from '~/assets/json/rules.json'
+import MCBoard from '~/components/mc/mc-board'
 import MCTitle from '~/components/mc/mc-title'
 import RulesCard from '~/components/rules/rules-card'
 
 export default {
   components: {
+    MCBoard,
     MCTitle,
     RulesCard
   },
