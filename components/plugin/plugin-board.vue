4cde04cb262e7139f2c582e7d04139962e13915b
manyimy <wong.manyi19@gmail.com>
2020-07-19 05:39:47 +0800

    Added Plugin Page

---
+++ components/plugin/plugin-board.vue
@@ -0,0 +1,34 @@
+<template>
+  <MCBoard>
+    <MCTitle class="mt-4" :text-shadow="true">
+      Plugins
+    </MCTitle>
+    <div>
+      <ul>
+        <li v-for="plugin in plugins" :key="plugin">
+          <p>
+            {{ plugin }}
+          </p>
+        </li>
+      </ul>
+    </div>
+  </MCBoard>
+</template>
+
+<script>
+import MCBoard from '~/components/mc/mc-board'
+import MCTitle from '~/components/mc/mc-title'
+import { plugins } from '~/assets/json/plugins.json'
+
+export default {
+  components: {
+    MCBoard,
+    MCTitle
+  },
+  data: () => {
+    return {
+      plugins
+    }
+  }
+}
+</script>
