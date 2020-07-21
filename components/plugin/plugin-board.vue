77b0fda7a4ce998250479e02969373228a3acfae
TajamHeng <oojinheng@gmail.com>
2020-07-22 02:56:58 +0800

    add a bit styling

--- components/plugin/plugin-board.vue
+++ components/plugin/plugin-board.vue
@@ -1,24 +1,28 @@
 <template>
   <MCBoard>
-    <MCTitle>
-      Plugins
-    </MCTitle>
-    <div>
-      <ul>
-        <li
-          v-for="plugin in plugins"
-          :key="`plugin-${plugin}`"
-          class="my-1"
-        >
-          <a
-            :href="plugin.href ? plugin.href : ''"
-            target="_blank"
-          >
-            {{ plugin.text }}
-          </a>
-        </li>
-      </ul>
-    </div>
+    <b-row align-h="center">
+      <b-col cols="12" md="8">
+        <MCTitle>
+          Plugins
+        </MCTitle>
+        <div>
+          <ul>
+            <li
+              v-for="plugin in plugins"
+              :key="`plugin-${plugin}`"
+              class="my-1"
+            >
+              <a
+                :href="plugin.href ? plugin.href : ''"
+                target="_blank"
+              >
+                {{ plugin.text }}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </b-col>
+    </b-row>
   </MCBoard>
 </template>
 
@@ -48,6 +52,12 @@ ul {
 }
 
 a {
+  color: rgba(255, 255, 255, 1);
+  transition: 250ms;
+}
+
+a:hover {
+  text-shadow: 1px 1px 2px rgba(255, 255, 255, .5);
   text-decoration: none;
   cursor: pointer;
 }
